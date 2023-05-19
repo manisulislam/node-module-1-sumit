4eb94df335fb01648e97075f2b07b593aec2f779
anisul islam <anisulislam980340@gmail.com>
2023-05-19 10:37:25 +0600

    global and module system concept updated

--- index.js
+++ index.js
@@ -3,4 +3,6 @@
     
 // }, 1000);
 const people = require('./people');
-console.log(people);
+console.log(people.test);
+console.log(people.people);
+people.anis();
--- people.js
+++ people.js
@@ -1,5 +1,5 @@
 
-(function(exports,require, module, __dirname, __filename){
+// (function(exports,require, module, __dirname, __filename){
 
     const people =['anis', 'ahad', 'jahed'];
     const test= 5;
@@ -7,9 +7,13 @@
         console.log("hello world");
     }
     console.log(module)
-    module.exports= people;
-    return module.exports;
-})
+    module.exports= {
+        people,
+        anis,
+        test
+    };
+    // return module.exports;
+// })
 
 /*
 note:
